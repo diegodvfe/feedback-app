349635f19ff831ca59ae477302094ad8e407d1c0
Diego <diego.lopez.19@hotmail.es>
2022-06-29 12:21:03 -0600

    new componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 
-function App(){
-const title = 'Blog Post'
-const body = 'This is my blog post'
-const comments = [
-  {id: 1, text: 'Comment one'},
-  {id: 2, text: 'Comment two'},
-  {id: 3, text: 'Comment three'},
-]
+import Header from "./components/Header"
 
-  return(
+function App (){
+  return (
+    <>
+  <Header text={true} />
     <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      <div className="comments">
-        <h3 >Coments ({comments.length})</h3>
-        <ul>
-          
-        </ul>
-      </div>
+      <h1>My app</h1>
     </div>
-    )
-  }
+    </>
+  )
+}
 
-  export default App;
+export default App
