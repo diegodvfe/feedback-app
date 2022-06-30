ff7a629f60a39fe5c6c22a4e081ef41dc884ee01
Diego <diego.lopez.19@hotmail.es>
2022-06-30 11:09:54 -0600

    add data

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+function FeedbackItem({item}) {
+
+   // THIS IS COMPONENT THAT HANDLE THE STATE OF THE SPECIFIC COMPONENT
+  // const handleClick = () =>{
+  //   setRating((prev) =>{
+  //     return prev + 1
+  //   })
+  // }
+  // <button onClick={handleClick}>Click</button>
+
+  return (
+    <div className='card'>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
+    </div>
+  )
+}
+
+export default FeedbackItem
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,31 @@
+
+
+const FeedbackData = [
+  {
+   id: 1,
+    rating: 10,
+    text: 'How is your food'
+  },
+  {
+    id:2,
+    rating: 9,
+    text: 'Which is the best food you have in your life'
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'How good is the taste of our plates?'
+  },
+  {
+    id: 4,
+    rating: 8,
+    text: 'This was not good and enough'
+  },
+  {
+    id: 5,
+    rating: 7,
+    tex:'Necesita ayuda en vida',
+  },
+]
+
+export default FeedbackData
