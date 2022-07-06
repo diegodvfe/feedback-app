576600c36a90a9e8a670c9441ea5de06820d684b
Diego <diego.lopez.19@hotmail.es>
2022-07-06 10:59:11 -0600

    btn-component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,24 @@
 import Card from "./shared/Card"
 import { useState } from "react"
+import Button from "./Button"
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handletextChange = (e) =>{
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    }else if(text !== '' && text.trim().length <= 10){
+      setMessage('Text must be at at least 10 characters')
+      setBtnDisabled(true);
+    }else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+
     setText(e.target.value)
   }
 
@@ -20,8 +34,11 @@ function FeedbackForm() {
            placeholder="Write a review"
            value={text}
            />
-          <button type="submit"> Send </button>
+          <Button type="submit" isDisabled={btnDisabled}> Send </Button>
         </div>
+
+           {message && <div className="message">{message}</div>}
+
       </form>
     </Card>
   )
