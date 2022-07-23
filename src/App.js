f8208f726fe753a7f765acbd69c9198599b01ab0
diegodvfe <diego.lopez.19@hotmail.es>
2022-07-23 10:56:40 -0800

    routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import {v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from './pages/AboutPage'
 
-
-
+import FeedbackData from "./data/FeedbackData"
 
 function App (){
   const [feedback, setFeedback] = useState(FeedbackData)
 
   const addFeedback = (newFeedback) =>{
-    newFeedback.d = uuidv4()
+    newFeedback.id = uuidv4()
     // console.log(newFeedback)
     setFeedback([newFeedback, ...feedback])
   }
 
    const deleteFeedback = (id) =>{
-    if (window.confirm('Esta sseguro de eliminar ?')){
+    if (window.confirm('Esta seguro de eliminar ?')){
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
   return (
-    <>
-  <Header  />
-    <div className="container">
-      <FeedbackForm handleAdd={addFeedback} />
-      <FeedbackStats feedback={feedback} />
-       <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-
-    </div>
-    </>
+    <Router>
+      <Header  />
+        <div className="container">
+          <Route exact path='/home'>
+          <FeedbackForm handleAdd={addFeedback} />
+          <FeedbackStats feedback={feedback} />
+          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+          </Route>
+        <Route path='/about' element={<AboutPage />}/>
+        </div>
+    </Router>
   )
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import Card from "../components/shared/Card"
+
+
+
+function AboutPage() {
+  return <Card>
+
+    <div className="">About</div>
+      <h1>About this project</h1>
+      <p>Please leave a comment for a product or service </p>
+      <p> Version 1.0 </p>
+      <p>
+        <a href="/"> Back to home</a>
+      </p>
+  </Card>
+}
+
+export default AboutPage
