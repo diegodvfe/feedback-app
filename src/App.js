a5e6f2e12a304d54455e2502084fd4fc8d83cb59
diegodvfe <diego.lopez.19@hotmail.es>
2022-07-24 14:17:25 -0800

    refact

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-
-
-import { BrowserRouter as Router } from 'react-router-dom'
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackStats from "./components/FeedbackStats"
-import FeedbackForm from "./components/FeedbackForm"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage'
-
 import { FeedbackProvider } from './context/FeedbackContext'
-import AboutLink from './components/AboutLink';
 
-function App (){
-
-
+function App() {
   return (
     <FeedbackProvider>
-    <Router>
-      <Header  />
-      {/* <Routes> */}
-        
-        <div className="container">
-          {/* <Route exact path='/home'> */}
-          <FeedbackForm  />
-          <FeedbackStats />
-          <FeedbackList   />
-          {/* </Route> */}
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            >
 
+            </Route>
 
-        {/* <Route path='/about' component={AboutPage }/> */}
-        <AboutLink />
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
         </div>
-
-      {/* </Routes> */}
-    </Router>
-   </FeedbackProvider> 
+      </Router>
+    </FeedbackProvider>
   )
 }
 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,19 +1,20 @@
-import Card from "../components/shared/Card"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Card from '../components/shared/Card'
 
+function AboutPage(props) {
+  return (
+    <Card>
+      <div className='about'>
+        <h1>About This Project</h1>
+        <p>This is a React app to leave feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
 
-
-function AboutPage() {
-  return <Card>
-
-    <div className="">About</div>
-      <h1>About this project</h1>
-      <p>Please leave a comment for a product or service </p>
-      <p> Version 1.0 </p>
-      <p>
-        <Link href="/"> Back to home</Link>
-      </p>
-  </Card>
+        <p>
+          <Link to='/'>Back To Home</Link>
+        </p>
+      </div>
+    </Card>
+  )
 }
 
 export default AboutPage
